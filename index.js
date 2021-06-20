7eb46e25b770a70f9b4f8627ca86211389ac56b1
Moumen Hallak <moumen95@gmail.com>
2021-06-20 19:56:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function submitData(name, email){
+<<<<<<< HEAD
     return fetch("http://localhost:3000/users", {
     method: "POST",
     headers: {
@@ -15,3 +16,20 @@ function submitData(name, email){
     });
   }
   
+=======
+  return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+},
+    body: JSON.stringify({name: name, email: email})
+  })
+  .then(resp => resp.json())
+  .then(data => document.body.innerHTML += `${data.id}`)
+  .catch(error => {
+    alert("Unauthorized Access");
+    document.body.innerHTML += `${error.message}`;
+  });
+}
+>>>>>>> 44d3bb40da96a44401670de1abbd2cff77f8d5c8
